3b9ee5110fdd37466a4fd92de3b60739eac88345
Aakash Dhar Dubey <aakashdhar.dubey@dotsquares.com>
2025-04-17 12:35:21 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -60,10 +60,10 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = 5000;
-  server.listen({
+  app.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+ 
   }, () => {
     log(`serving on port ${port}`);
   });
